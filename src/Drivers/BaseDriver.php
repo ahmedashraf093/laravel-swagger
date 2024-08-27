d0afa4026cf25b4018bcf8da89ca5234e53d09e3
vgrams <vgrams@ronasit.com>
2024-08-27 11:11:47 +0600

    feat: implemented locks to allow multiple processes to write temp data in one file;

--- src/Drivers/BaseDriver.php
+++ src/Drivers/BaseDriver.php
@@ -3,6 +3,7 @@
 namespace RonasIT\Support\AutoDoc\Drivers;
 
 use RonasIT\Support\AutoDoc\Interfaces\SwaggerDriverInterface;
+use RuntimeException;
 
 abstract class BaseDriver implements SwaggerDriverInterface
 {
@@ -15,24 +16,56 @@ abstract class BaseDriver implements SwaggerDriverInterface
 
     public function saveTmpData($data): void
     {
-        file_put_contents($this->tempFilePath, json_encode($data));
+        $this->withTmpFileHandle('c+', function ($handle) use ($data) {
+            $this->lockHandle($handle, LOCK_EX);
+
+            ftruncate($handle, 0);
+            rewind($handle);
+            fwrite($handle, json_encode($data, JSON_THROW_ON_ERROR));
+
+            flock($handle, LOCK_UN);
+        });
     }
 
     public function getTmpData(): ?array
     {
-        if (file_exists($this->tempFilePath)) {
-            $content = file_get_contents($this->tempFilePath);
+        return $this->withTmpFileHandle('r', function ($handle) {
+            $this->lockHandle($handle, LOCK_SH);
+
+            $content = stream_get_contents($handle);
+
+            flock($handle, LOCK_UN);
 
             return json_decode($content, true);
-        }
-
-        return null;
+        });
     }
 
     protected function clearTmpData(): void
     {
         if (file_exists($this->tempFilePath)) {
             unlink($this->tempFilePath);
+        }
+    }
+
+    protected function withTmpFileHandle(string $mode, callable $callback): mixed
+    {
+        $handle = @fopen($this->tempFilePath, $mode);
+
+        if ($handle === false) {
+            return null;
+        }
+
+        try {
+            return $callback($handle);
+        } finally {
+            fclose($handle);
+        }
+    }
+
+    protected function lockHandle($handle, int $operation): void
+    {
+        if (!flock($handle, $operation)) {
+            throw new RuntimeException("Failed to lock the file: {$this->tempFilePath}");
         }
     }
 }
